189b84f4c5939ab121b9bb58cc96d5dc519c9a37
Michealchan <michaelchankawhi@gmail.com>
2022-01-13 20:22:28 +0800

    完善配置

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -1,23 +1,29 @@
 const path = require('path');
 const TerserPlugin = require("terser-webpack-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const VueLoaderPlugin = require('vue-loader-plugin');
+const { VueLoaderPlugin } = require('vue-loader');
 const miniSVGDataURI = require("mini-svg-data-uri");
 module.exports = {
     entry: ['@babel/polyfill', './src/main.js'],
     resolve: {
-        extensions: ['', '.js', '.vue', '.scss', '.css'],
+        extensions: ['.vue','.js','.json','scss','css'],
         alias: {
             '@': path.resolve(__dirname, '../src'),
         }
     },
     module: {
-        rules: [{
+        rules: [
+            {
+                test: /\.vue$/i,
+                loader: 'vue-loader'
+            },
+            {
                 test: /\.m?js$/,
                 exclude: /node_modules/,
                 use: {
                     loader: "babel-loader",
                     options: {
+                        cacheDirectory: true,//当有设置时，指定的目录将用来缓存 loader 的执行结果。之后的 webpack 构建，将会尝试读取缓存，来避免在每次执行时，可能产生的、高性能消耗的 Babel 重新编译过程(recompilation process)
                         presets: [
                             ['@babel/preset-env', {
                                 useBuiltIns: 'usage',
@@ -36,36 +42,52 @@ module.exports = {
                     }
                 }
             },
+            // {
+            //     test: /\.(png|jpe?g|gif)$/i,
+            //     use: [{
+            //         loader: 'url-loader',
+            //         options: {
+            //             limit: 1024000,
+            //             name: '[name]_[hash].[ext]',
+            //             outputPath: 'image/'
+            //         }
+            //     }]
+            // },
             {
-                test: /\.vue$/i,
-                use: {
-                    loader: 'vue-loader'
-                }
+                test: /\.(png|svg|jpg|gif)$/,
+                type: 'asset',
+                parser: {
+                    dataUrlCondition: {
+                        maxSize: 8 * 1024,
+                    },
+                },
             },
             {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [{
-                    loader: 'url-loader',
-                    options: {
-                        limit: 1024000,
-                        name: '[name]_[hash].[ext]',
-                        outputPath: 'image/'
-                    }
-                }]
+                test: /\.html$/,
+                use: ['html-loader'],
+            },
+            // {
+            //     test: /\.svg$/,
+            //     loader: 'svg-sprite-loader',
+            //     options: {
+            //         symbolId: 'icon-[name]'
+            //     }
+            // },
+            // {
+            //     test: /\.(ttf|woff|woff2)$/i,
+            //     use: {
+            //         loader: 'file-loader'
+            //     }
+            // }
+            {
+                test: /\.(jpe?g|png|gif|svg)$/,
+                loader: 'image-webpack-loader',
+                enforce: 'pre',
             },
             {
-                test: /\.svg$/,
-                loader: 'svg-sprite-loader',
-                options: {
-                    symbolId: 'icon-[name]'
-                }
+                test: /\.(eot|woff|ttf|woff2|appcache|mp4|pdf)(\?|$)/,
+                type: 'asset/resource',
             },
-            {
-                test: /\.(ttf|woff|woff2)$/i,
-                use: {
-                    loader: 'file-loader'
-                }
-            }
 
         ]
     },
@@ -112,5 +134,8 @@ module.exports = {
         filename: '[name].[hash].bundle.js',
         chunkFilename: '[name].[hash].js',
         path: path.resolve(__dirname, '../dist')
+    },
+    performance: {
+        hints: false,//防止把体积巨大的 bundle存在在项目中，从而影响网页的性能。
     }
 }
