d07f38d7770f5d12dd43a0f26f57b895714aab95
Michealchan <michaelchankawhi@gmail.com>
2022-01-13 10:11:35 +0800

    优化终端

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import huawei from './huawei.png';
 // import './index.scss';
 // import './style.scss';
 // import '@babel/polyfill';
-import _ from 'lodash';
+// import _ from 'lodash';
 // import Vue from 'vue'
 // import ElementUI from 'element-ui'
 // import locale from 'element-ui/lib/locale/lang/en'
@@ -30,10 +30,10 @@ import App from './App.vue';
 // canvas.addEventListener('click', function (event) {
 
 //     context.fillStyle = '#fff';
-//     context.strokeStyle = '#fff'; 
-//     context.font = "bold 40px ,'微软雅黑','宋体'"; 
-//     // context.textBaseline = 'hanging'; 
-//     context.fillText(event.clientX, event.clientX - 200, event.clientY- 500); 
+//     context.strokeStyle = '#fff';
+//     context.font = "bold 40px ,'微软雅黑','宋体'";
+//     // context.textBaseline = 'hanging';
+//     context.fillText(event.clientX, event.clientX - 200, event.clientY- 500);
 //     console.log(event.clientX, event.clientY);
 // })
 // imgDom.src = huawei;
@@ -79,11 +79,11 @@ import App from './App.vue';
 //         console.log(_.join(['a', 'b', 'c'], '~'));
 //     })
 // }
-document.addEventListener('click', function() {
-    import('lodash').then(_ => {
-        console.log(_.join(['a', 'b', 'c'], '~'));
-    })
-})
+// document.addEventListener('click', function() {
+//     import('lodash').then(_ => {
+//         console.log(_.join(['a', 'b', 'c'], '~'));
+//     })
+// })
 new Vue({
 
     render: h => h(App)
