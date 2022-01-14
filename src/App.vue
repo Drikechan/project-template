6e84a60321768131048c001d39b75a0646cd918a
Michealchan <michaelchankawhi@gmail.com>
2022-01-14 15:57:00 +0800

    完善配置

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,30 @@
 <template>
   <div>
     <svg width="440" height="440" viewbox="0 0 440 440">
-      <circle cx="220" cy="220" r="170" stroke-width="10" stroke="#D1D3D7" fill="none"></circle>
+      <circle cx="220" cy="220" r="170" stroke-width="10" stroke="#D1D3D7" fill="none"  stroke-dasharray="267">
+        <animateTransform attributeName="transform" type="rotate" values="0 220 220;360 220 220" dur="1s" repeatCount="indefinite"></animateTransform>
+        <animate attributeName="stroke"  repeatCount="indefinite" values="#7FFFD4;#00A5E0;#7FFFD4;" dur="1s"></animate>
+      </circle>
       <circle
           class="circle"
           cx="220"
           cy="220"
-          r="170"
+          r="130"
           stroke-width="10"
           stroke="#00A5E0"
           fill="none"
-          transform="matrix(0,-1,1,0,0,440)"
-      />
+          stroke-dasharray="204"
+      >
+        <animateTransform attributeName="transform" type="rotate" values="360 220 220;0 220 220" dur="1s" repeatCount="indefinite"></animateTransform>
+        <animate attributeName="stroke"  repeatCount="indefinite" values="#00A5E0;#7FFFD4;#00A5E0;" dur="1s"></animate>
+      </circle>
     </svg>
-    <tab-index></tab-index>
   </div>
 </template>
 
 <script>
-import TabIndex from '@/Components/index.vue'
 export default {
   name: 'SvgAnimation',
-  components: {
-    TabIndex
-  },
   created() {
     this.handlerPage()
   },
@@ -39,16 +40,16 @@ export default {
 </script>
 
 <style scoped>
-.circle {
-  animation: circle 5s linear infinite;
-}
+/*.circle {*/
+/*  animation: circle 5s linear infinite;*/
+/*}*/
 
-@keyframes circle {
-  from {
-    stroke-dasharray: 0 1069;
-  }
-  to {
-    stroke-dasharray: 1069 0;
-  }
-}
+/*@keyframes circle {*/
+/*  from {*/
+/*    stroke-dasharray: 0 1069;*/
+/*  }*/
+/*  to {*/
+/*    stroke-dasharray: 1069 0;*/
+/*  }*/
+/*}*/
 </style>
